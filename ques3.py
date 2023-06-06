f620fac32d3945ce10aa9568c43d3b25ad7e3a83
zeba2004 <117371677+zeba2004@users.noreply.github.com>
2023-06-06 21:37:00 +0530

    Create ques3.py

---
+++ ques3.py
@@ -0,0 +1,10 @@
+# Sides of the triangle are given
+a = 5
+b = 6
+c = 7
+
+# Calculating the perimeter of the triangle
+perimeter=a+b+c
+
+# Print statement
+print("Perimeter of triangle is %d",perimeter)
